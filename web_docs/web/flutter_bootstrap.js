c4091b2f188d8005e1f02a3592e305f5292e7701
Parth U <parth@clucloud.com>
2025-10-03 16:14:21 +0530

    inital commit: sdk: 3.22.3 generate apk and add into read me file

--- web_docs/web/flutter_bootstrap.js
+++ web_docs/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3556416587"
+    serviceWorkerVersion: "3249944450"
   }
 });
